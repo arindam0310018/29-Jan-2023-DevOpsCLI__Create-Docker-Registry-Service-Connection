a08ca31375005a1ffac0e32dacc4dfe966208582
Arindam Mitra <mail2arindam2003@yahoo.com>
2023-01-30 00:38:55 +0100

    Update Create-Docker-Registry-DevOps-Service-Connection-Without-Prompting-PAT.ps1

--- Create-Docker-Registry-DevOps-Service-Connection-Without-Prompting-PAT.ps1
+++ Create-Docker-Registry-DevOps-Service-Connection-Without-Prompting-PAT.ps1
@@ -23,4 +23,3 @@ az devops service-endpoint create --service-endpoint-configuration .\$configJSON
 # Grant Access to all Pipelines to the Newly Created Docker Registry DevOps Service Connection:-
 $srvEndpointID = az devops service-endpoint list --query "[?name=='$dockersrvconn'].id" -o tsv
 az devops service-endpoint update --id $srvEndpointID --enable-for-all
-
